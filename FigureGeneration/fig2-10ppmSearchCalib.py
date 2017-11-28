fc0d15220622c223667b983e26114a6a2f168672
stefanks <solntsev@wisc.edu>
2017-11-28 15:11:11 -0600

    djfkdj

--- FigureGeneration/fig2-10ppmSearchCalib.py
+++ FigureGeneration/fig2-10ppmSearchCalib.py
@@ -5,10 +5,10 @@ import pandas
 maxi = 35000 
 
 
-height = 6
+height = 3
 width =  7 
 
-f, ((ax1), (ax3)) = plt.subplots(2, 1, sharex='col', sharey='row', figsize=(width,height))
+f, ((ax1), (ax3)) = plt.subplots(1, 2, figsize=(width,height))
 
 ok = pandas.read_csv(r"C:\Users\stepa\Data\PaperData\JurkatTrypsin\1mm10ppmSearch\Task1-SearchTask\aggregatePSMs_1mm.psmtsv", sep='\t')  
 
@@ -30,11 +30,8 @@ ax1.hist(A[(C<0.01) & B], bins  = 101, range=[-10.1,10.1])
 ax1.set_ylim([1,maxi])
 ax1.vlines(0,0,maxi,zorder = 10)
 ax1.set_ylabel('PSM count')
-ax1.annotate(r"Uncalibrated", xy=(0, 0.5), xycoords=ax1.yaxis.label, textcoords='offset points',
-                size='large', ha='right', va='center', xytext=(0,0.5))
-
-
-
+ax1.set_xlabel('Mass Error (ppm)')
+ax1.set_title('Uncalibrated')
 
 ok = pandas.read_csv(r"C:\Users\stepa\Data\PaperData\JurkatTrypsin\Calibrated\1mm10ppm\Task1-SearchTask\aggregatePSMs_1mm.psmtsv", sep='\t')  
 
@@ -57,15 +54,10 @@ ax3.set_ylim([1,maxi])
 ax3.vlines(0,0,maxi,zorder = 10)
 ax3.set_ylabel('PSM count')
 ax3.set_xlabel('Mass Error (ppm)')
-
-ax3.annotate(r"Calibrated", xy=(0, 0.5), xycoords=ax3.yaxis.label, textcoords='offset points',
-                size='large', ha='right', va='center', xytext=(0,0.5))
+ax3.set_title('Calibrated')
 
 
 plt.tight_layout()
-
-plt.subplots_adjust(left=0.3)
-
 
 plt.savefig(r'C:\Users\stepa\Source\StefanPaper\fig2-10ppmSearchCalib.png', format='png', dpi=600)
 
