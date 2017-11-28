8634684d94ac2abd3ae67aab266afce7091ee534
stefanks <solntsev@wisc.edu>
2017-11-28 13:31:40 -0600

    alright

--- FigureGeneration/fig3-searchTypes.py
+++ FigureGeneration/fig3-searchTypes.py
@@ -13,7 +13,7 @@ fig = plt.figure(figsize=(width, height))
 ax = fig.add_subplot(131)
 for p in [
     patches.Rectangle(
-        (-105, 0), 101, 1,
+        (-110, 0), 106, 1,
         alpha=0.4,
         facecolor="red"
     ),
@@ -21,7 +21,7 @@ for p in [
         (-4, 0), 8, 1,
     ),
     patches.Rectangle(
-        (4, 0), 101, 1,
+        (4, 0), 106, 1,
         alpha=0.4,
         facecolor="red"
     )
@@ -29,15 +29,10 @@ for p in [
     ax.add_patch(p)
 
 
-ax.set_xlim([-105,105])
+ax.set_xlim([-110,110])
 ax.set_ylim([-2, 9])
 
-ax.set_xlabel('Mass Diff (Da)')
-
-ax.set_xlabel('Mass Diff (Da)')
-
-
-plt.annotate('Only mass differences\nclose to zero allowed', xy=(0,1), xytext=(-80,4), arrowprops=dict(arrowstyle="->"))
+plt.annotate('Only mass differences\nclose to zero allowed', xy=(0,1), xytext=(-106,4), arrowprops=dict(arrowstyle="->"))
 plt.title('Narrow-Window Search')
 
 
@@ -48,7 +43,7 @@ ax.get_yaxis().set_visible(False)
 ax = fig.add_subplot(132)
 for p in [
     patches.Rectangle(
-        (-105, 0), 5, 1,
+        (-110, 0), 10, 1,
         alpha=0.4,
         facecolor="red"
     ),
@@ -56,7 +51,7 @@ for p in [
         (-100, 0), 200, 1,
     ),
     patches.Rectangle(
-        (100, 0), 5, 1,
+        (100, 0), 10, 1,
         alpha=0.4,
         facecolor="red"
     )
@@ -64,14 +59,11 @@ for p in [
     ax.add_patch(p)
 
 
-ax.set_xlim([-105,105])
+ax.set_xlim([-110,110])
 ax.set_ylim([-2, 9])
 
-ax.set_xlabel('Mass Diff (Da)')
 
-ax.set_xlabel('Mass Diff (Da)')
-
-plt.annotate('Wide range of mass\ndifferences allowed', xy=(0,1), xytext=(-80,4), arrowprops=dict(arrowstyle="->"))
+plt.annotate('Wide range of mass\ndifferences allowed', xy=(0,1), xytext=(-100,4), arrowprops=dict(arrowstyle="->"))
 ax.annotate("", xy=(-50, 1), xytext=(-10, 3.3), arrowprops=dict(arrowstyle="->"))
 ax.annotate("", xy=(50, 1), xytext=(10, 3.3), arrowprops=dict(arrowstyle="->"))
 plt.title('Open Search')
@@ -83,7 +75,7 @@ ax.get_yaxis().set_visible(False)
 ax = fig.add_subplot(133)
 for p in [
     patches.Rectangle(
-        (-105, 0), 101, 1,
+        (-110, 0), 106, 1,
         alpha=0.4,
         facecolor="red"
     ),
@@ -107,7 +99,7 @@ for p in [
         (76, 0), 8, 1,
     ),
     patches.Rectangle(
-        (84, 0), 21, 1,
+        (84, 0), 26, 1,
         alpha=0.4,
         facecolor="red"
     )
@@ -115,12 +107,10 @@ for p in [
     ax.add_patch(p)
 
 
-ax.set_xlim([-105,105])
+ax.set_xlim([-110,110])
 ax.set_ylim([-2, 9])
 
-ax.set_xlabel('Mass Diff (Da)')
-
-plt.annotate('Only specific mass\ndifferences allowed', xy=(0,1), xytext=(-80,4), arrowprops=dict(arrowstyle="->"))
+plt.annotate('Only specific mass\ndifferences allowed', xy=(0,1), xytext=(-95,4), arrowprops=dict(arrowstyle="->"))
 ax.annotate("", xy=(42, 1), xytext=(10, 3.3), arrowprops=dict(arrowstyle="->"))
 ax.annotate("", xy=(80, 1), xytext=(20, 3.3), arrowprops=dict(arrowstyle="->"))
 
@@ -128,9 +118,26 @@ plt.title('Multi-Notch Search')
 
 ax.get_yaxis().set_visible(False)
 
+################
+
+fig.text(0.45, 0.00, 'mass differences between observed experimental mass and theoretical candidate mass', ha='center')
+
+#add red blue legend to plot
+#global x axis label instead of the three mass diff da 
+#mass differences between observed experimental mass and theoretical candidate mass
+
+
 #######
 
+blue_patch = patches.Patch(label='Allowed')
+red_patch = patches.Patch(color='red', label='Not\nAllowed', alpha=0.4)
+
+plt.legend(handles=[blue_patch, red_patch],bbox_to_anchor=(1.05, 0.8), loc=2, borderaxespad=0.)
+
+
 plt.tight_layout()
+
+plt.subplots_adjust(right=0.83)
 
 plt.savefig(r'C:\Users\stepa\Source\StefanPaper\fig3-searchTypes.png', format='png', dpi=600)
 
